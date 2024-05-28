1ab40411af4a794ae3f3145b551bf73861f4c4bd
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2024-05-28 19:38:31 +0530

    Add files via upload

---
+++ second.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void display()
+
+{
+	printf("My name is mayuri..\n");
+}
+int main()
+{
+	printf("Inside main function..\n");
+
+	display();
+
+
+	printf("End of the main function..");
+
+	return 0;
+}
