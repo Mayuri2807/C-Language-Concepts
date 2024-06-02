c97cfb830488a059bf19479b8e75bb23f98a08ee
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2024-06-02 20:25:57 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int no=0;
+    int ans=0;
+    printf("enter the number..\n");
+    scanf("%d",&no);
+    ans=no%2;
+    if(ans==0)
+    {
+      printf("number is even... \n");
+    }
+    else
+    {
+      printf("number is odd...\n");
+    }
+	return 0;
+}
