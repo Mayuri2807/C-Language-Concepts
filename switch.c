29af44749be3d48117eb4ed3a5b76a1e5154816f
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2024-06-03 22:44:17 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+
+{
+	int standard=0;
+	printf("Enter the standard ...\n");
+	scanf("%d",&standard);
+	switch(standard)
+	{
+	  case 1:
+	  printf("your exam at 7 am..\n");
+	  break;
+	  case 2:
+	  printf("your exam at 8 am...\n");
+	  break;
+	  case 3:
+	  printf("your exam at 9 am..\n");
+	  break;
+	  default:
+	  printf("wrong choice...\n");
+    }
+	return 0;
+}
