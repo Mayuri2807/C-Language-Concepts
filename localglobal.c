e0fb7c7fe7cca6321c87ad9cba941db31567a09a
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2024-06-07 20:05:12 +0530

    Add files via upload

---
+++ localglobal.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int x= 101;
+
+void fun()
+{
+  int i=21;
+  int no= 51;
+
+  printf("from fun value of no is:%d\n",no);
+  printf("from fun value of no is:%d\n",i);
+   printf("from fun value of no is:%d\n",x);
+}
+int main()
+
+{
+	int no= 11;
+	printf("from fun value of no is:%d\n",no);
+	printf("from fun value of no is:%d\n",x);
+}
